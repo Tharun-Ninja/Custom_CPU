559aba0f8fe13f506f0ffa9d451baf4a5eb473c7
Tharun <tharunharish84@gmail.com>
2023-04-24 21:25:22 +0530

    working on converting ins to machine code

--- assembler.py
+++ assembler.py
@@ -1,7 +1,15 @@
 #! python3
+import sys
+import os
 
-def get_register_address(register):
-    registers = {
+
+def print_help():
+    print("Usage: python3 assembler.py <text_file>")
+    
+def print_error(error):
+    print(f"Error: {error}")
+    
+registers = {
         "R0": "000",
         "R1": "001",
         "R2": "010",
@@ -11,9 +19,21 @@ def get_register_address(register):
         "R6": "110",
         "FLAGS": "111"
     }
-    return registers[register]
+
+
+def get_register_address(reg):
+    global registers
+    if reg in registers:
+        return registers[reg], 0
+    else:
+        print_error('Register not Found')
+        return "", 1
+
+def is_reg(register):
+    return 1 if register[reg] else 0
     
-def get_ins_opcode(ins, type):
+def get_ins_opcode(ins, ins_type):
+    print(ins)
     instructions = {
         "A": {
             "add": "00000",
@@ -49,6 +69,92 @@ def get_ins_opcode(ins, type):
         }
     }
     
-    return instructions[type][ins]
+    if ins_type in instructions and ins in instructions[ins_type]:
+        return instructions[ins_type][ins] , 0
+    else:
+        print_error("Instruction not Found")
+        return "" , 1
+
+
+
+def get_type(ins):
+    if len(ins) == 1:
+        return "F"
     
-print(get_register_address("R3"))
+    elif len(ins) == 2:
+        return "E"
+    
+    elif len(ins) == 3:
+        if ins[-1][0] == "$":
+            return "B"
+        elif is_reg(ins[-1]):
+            return "C"
+        elif 1:
+            return "D" # need to check mem type 
+        else:
+            # Throw error
+            pass
+        
+    elif len(ins) == 4:
+        return "A"
+    
+    else:
+        # Throw error
+        pass
+    
+def create_ins_bits(ins, ins_type):
+    bit_ins = ""
+    check = 0
+    if ins_type == "A":
+        print('hi')
+        try:
+            print(ins[0], ins_type)
+            bit_ins, check = get_ins_opcode(ins[0], ins_type)
+            print(bit_ins, check)
+            return bit_ins, check
+        except:
+            exit()
+    elif ins_type == "B":
+        print('hi')
+        try:
+            print(ins[0], ins_type)
+            bit_ins, check = get_register_address(ins[1])
+            print(bit_ins, check)
+            return bit_ins, check
+        except:
+            pass
+            # exit()
+        
+        
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print_help()
+        exit(1)
+
+    filename = sys.argv[1]
+    
+    if filename not in os.listdir():
+        print_error("File not found")
+        exit(1)
+
+    program_counter = 0
+    current_ins = ""
+    # with open(filename) as f:
+    #     for line in f:
+    #         line = line.strip().split(' ')
+    #         program_counter += 1
+    #         print(line)
+    #         if line[0] != 'var' and line[0] != '':
+    #             ins_type = get_type(line)
+    #             current_ins = create_ins_bits(line, ins_type)
+                
+
+    # print(f"Total lines: {program_counter}")
+    
+    current_ins, check = create_ins_bits(['mov', 'R22', '$10'], "B")
+    print(current_ins, check)
+    print("hihsf")
+    
+        
