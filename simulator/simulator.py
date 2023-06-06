1d6b16f1f16841a47590a877a5ddc409e0075342
Tharun <tharunharish84@gmail.com>
2023-06-06 22:36:25 +0530

    bug fixes

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -30,56 +30,57 @@ class Memory:
             # print(ins)
             # f.write(ins + "\n")
             sys.stdout.write(f"{ins}\n")
-    class Opcodes:
-        instructions = {
-        "A": {
-            "add": "00000",
-            "sub": "00001",
-            "mul": "00110",
-            "xor": "01010",
-            "or": "01011",
-            "and": "01100",
-            "addf": "10000",
-            "subf": "10001",
-            "rsb": "10100"
-        },
-        "B": {
-            "mov": "00010",
-            "rs": "01000",
-            "ls": "01001",
-            "movf": "10010",
-            "dec": "10110",
-            "inc": "10011",
-            "cmn": "10111" 
             
-        },
-        "C": {
-            "mov": "00011",
-            "div": "00111",
-            "not": "01101",
-            "cmp": "01110"
-        },
-        "D": {
-            "ld": "00100",
-            "st": "00101",
-            "swp": "10101"
-        },
-        "E": {
-            "jmp": "01111",
-            "jlt": "11100",
-            "jgt": "11101",
-            "je": "11111"
-        },
-        "F": {
-            "hlt": "11010"
-        }
+class Opcodes:
+    instructions = {
+    "A": {
+        "add": "00000",
+        "sub": "00001",
+        "mul": "00110",
+        "xor": "01010",
+        "or": "01011",
+        "and": "01100",
+        "addf": "10000",
+        "subf": "10001",
+        "rsb": "10100"
+    },
+    "B": {
+        "mov": "00010",
+        "rs": "01000",
+        "ls": "01001",
+        "movf": "10010",
+        "dec": "10110",
+        "inc": "10011",
+        "cmn": "10111" 
+        
+    },
+    "C": {
+        "mov": "00011",
+        "div": "00111",
+        "not": "01101",
+        "cmp": "01110"
+    },
+    "D": {
+        "ld": "00100",
+        "st": "00101",
+        "swp": "10101"
+    },
+    "E": {
+        "jmp": "01111",
+        "jlt": "11100",
+        "jgt": "11101",
+        "je": "11111"
+    },
+    "F": {
+        "hlt": "11010"
     }
-    
-    def getIns(self, opcode):
-        for types in self.instructions:
-            for ins in self.instructions[types]:
-                if self.instructions[types][ins] == opcode:
-                    return types, ins
+}
+
+def getIns(self, opcode):
+    for types in self.instructions:
+        for ins in self.instructions[types]:
+            if self.instructions[types][ins] == opcode:
+                return types, ins
 
 class RegisterFile:
     register_memory = ["0"*16 for _ in range(8)]
