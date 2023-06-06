52b9d5fd336e27277d1199511c5809c704451766
Tharun <tharunharish84@gmail.com>
2023-06-07 02:34:56 +0530

    code updates

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -1,18 +1,18 @@
 #! python3
 import sys
 
-# f = open('output.txt', 'w')
+f = open('output.txt', 'w')
 
 class Memory:
     mem = []
     
     def initialize(self):
-        # with open("./test.txt") as f:
-        #     for line in f:
-        #         self.mem.append(line.strip('\n'))
+        with open("./test.txt") as f:
+            for line in f:
+                self.mem.append(line.strip('\n'))
                 
-        for ins in sys.stdin:
-            self.mem.append(ins.strip())
+        # for ins in sys.stdin:
+        #     self.mem.append(ins.strip())
             
         if(len(self.mem) < 128):
             for i in range(len(self.mem), 128):
@@ -28,59 +28,59 @@ class Memory:
     def dump(self):
         for ins in self.mem:
             # print(ins)
-            # f.write(ins + "\n")
-            sys.stdout.write(f"{ins}\n")
+            f.write(ins + "\n")
+            # sys.stdout.write(f"{ins}\n")
             
 class Opcodes:
     instructions = {
-    "A": {
-        "add": "00000",
-        "sub": "00001",
-        "mul": "00110",
-        "xor": "01010",
-        "or": "01011",
-        "and": "01100",
-        "addf": "10000",
-        "subf": "10001",
-        "rsb": "10100"
-    },
-    "B": {
-        "mov": "00010",
-        "rs": "01000",
-        "ls": "01001",
-        "movf": "10010",
-        "dec": "10110",
-        "inc": "10011",
-        "cmn": "10111" 
-        
-    },
-    "C": {
-        "mov": "00011",
-        "div": "00111",
-        "not": "01101",
-        "cmp": "01110"
-    },
-    "D": {
-        "ld": "00100",
-        "st": "00101",
-        "swp": "10101"
-    },
-    "E": {
-        "jmp": "01111",
-        "jlt": "11100",
-        "jgt": "11101",
-        "je": "11111"
-    },
-    "F": {
-        "hlt": "11010"
+        "A": {
+            "add": "00000",
+            "sub": "00001",
+            "mul": "00110",
+            "xor": "01010",
+            "or": "01011",
+            "and": "01100",
+            "addf": "10000",
+            "subf": "10001",
+            "rsb": "10100"
+        },
+        "B": {
+            "mov": "00010",
+            "rs": "01000",
+            "ls": "01001",
+            "movf": "10010",
+            "dec": "10110",
+            "inc": "10011",
+            "cmn": "10111" 
+            
+        },
+        "C": {
+            "mov": "00011",
+            "div": "00111",
+            "not": "01101",
+            "cmp": "01110"
+        },
+        "D": {
+            "ld": "00100",
+            "st": "00101",
+            "swp": "10101"
+        },
+        "E": {
+            "jmp": "01111",
+            "jlt": "11100",
+            "jgt": "11101",
+            "je": "11111"
+        },
+        "F": {
+            "hlt": "11010"
+        }
     }
-}
 
-def getIns(self, opcode):
-    for types in self.instructions:
-        for ins in self.instructions[types]:
-            if self.instructions[types][ins] == opcode:
-                return types, ins
+    def getIns(self, opcode):
+        for types in self.instructions:
+            for ins in self.instructions[types]:
+                if self.instructions[types][ins] == opcode:
+                    return types, ins
 
 class RegisterFile:
     register_memory = ["0"*16 for _ in range(8)]
@@ -100,11 +100,11 @@ class RegisterFile:
         # print(self.register_memory)
         for d in self.register_memory:
             # print(d, end=" ")
-            # f.write(d + " ")
-            sys.stdout.write(f"{d} ")
+            f.write(d + " ")
+            # sys.stdout.write(f"{d} ")
             
         # print()
-        # f.write("\n")
+        f.write("\n")
         sys.stdout.write(f"\n")
         
         
@@ -115,13 +115,13 @@ class RegisterFile:
         self.reg_write(7, "0"*16)    
     
 class ProgramCounter:
-    def _init_(self, counter):
+    def __init__(self, counter):
         self.counter = counter
         
     def dump(self):
         # print(f"{self.counter:07b}", end=" ")
-        sys.stdout.write(f"{self.counter:07b} ")
-        # f.write(f"{self.counter:07b}" + " ")
+        # sys.stdout.write(f"{self.counter:07b} ")
+        f.write(f"{self.counter:07b}" + " ")
         
     def update(self, counter):
         self.counter = counter
@@ -238,7 +238,7 @@ class ExecutionEngine:
                     if reg3 > reg2:
                         RF.reg_write(self.bin_int('111'), self.int_bin(8))
                     else:
-                        RF.reg_write(self.bin_int(instruction[7:10]), self.dec_ieee(reg1))
+                        RF.reg_write(self.bin_int(instruction[7:10]), f"{'0'*8}{self.dec_ieee(str(reg1))}")
                     return False, PC.counter + 1
 
         elif type_ins == "B":
